08c29c3fd623359f03cc0469ceda0e810f170a6a
Shahid-ahmad-hajam <shahid.hajam2004@gmail.com>
2026-07-13 12:44:16 +0530

    Made connection related changes

--- cursor.py
+++ cursor.py
@@ -1,14 +1,16 @@
 import mysql.connector
 '''
-db: A variable that stores the connection object. This object is primary interface for all further 
-database operations. db is used to create cursors and manage transactions.'''
+db:-
+A variable that stores the connection object. 
+This object is primary interface for all further database operations. 
+db is used to create cursors and manage transactions.'''
 
 # db->Database connection Object
 db = mysql.connector.connect( #Creates a connection between Python program and MySQL database server.
     host="localhost",
-    user="root",  # Replace with MySQL username
-    password="Shahid@123",  # Replace with MySQL password
-    database="shahid1"  # Replace with database name
+    user="root",  # MySQL username
+    password="Shahid@123",  # MySQL password
+    database="shahid1"  # database name
 )
 
 cursor = db.cursor() # Database Cursor Object ->A cursor is like a pointer inside the database connection.
